f591f92d9ed54173f84384b9fdcda8433e26169d
Kasra <kasrakarimaian84@gmail.com>
2026-08-11 22:55:24 +0330

    create view profile

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.profiles, name="profiles"), #if its blank '' it means its become your homepage
-
+    path('profile/<str:pk>', views.userprofile, name='user-profile')
 ]
--- users/views.py
+++ users/views.py
@@ -6,3 +6,8 @@ def profiles(request):
     profiles = profile.objects.all()
     context = {'profiles':profiles}
     return render(request, 'users/profiles.html', context)
+
+def userprofile(request,pk):
+    userprofile = profile.objects.get(id=pk)
+    context = {'userprofile':userprofile}
+    return render(request, 'users/user-profile.html', context)
