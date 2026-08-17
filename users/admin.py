a37cc0f1fb24bea759e1cbec9a982cf000e71259
Kasra <kasrakarimaian84@gmail.com>
2026-08-17 18:44:48 +0330

    add models for messaging and change admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import profile,Skill
+from .models import profile,Skill,Message
 # for showing profile on admin panel
 admin.site.register(profile)
 admin.site.register(Skill)
+admin.site.register(Message)
