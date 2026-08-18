e1e10c4744899a3c7bbb55a7e30ef0346be54c72
Kasra <kasrakarimaian84@gmail.com>
2026-08-18 13:07:12 +0330

    some bugs fixed

--- users/signals.py
+++ users/signals.py
@@ -3,6 +3,8 @@ from .models import profile
 # we add this for django post save signals
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
 # function that create a profile anytime user sign up
 @receiver(post_save, sender=User)
@@ -18,4 +20,5 @@ def createProfile(sender, instance, created, **kwargs):
 # post_save.connect(createProfile, sender=User) we can use decorator instead of this
 # because of that cascade in profile anytime user deleted it profile will delete too and there is no need to delete function
 
+    
 
