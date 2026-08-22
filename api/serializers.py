575d01ffebcaaaef7ec7d17b46783015a164e65c
Kasra <kasrakarimaian84@gmail.com>
2026-08-22 21:40:34 +0330

    new serilization

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
-from projects.models import Project
+from projects.models import Project, tag
+from users.models import profile
+
+
+
+class ProfileSerializers(serializers.ModelSerializer):
+    class Meta :
+        model = profile
+        fields = '__all__'
+
+
+class TagSerializers(serializers.ModelSerializer):
+    class Meta :
+        model = tag
+        fields = '__all__'
+
 
 # this would converts our data in databse to json file
 class ProjectSerializers(serializers.ModelSerializer):
+    owner = ProfileSerializers(many = False)
+    tags = TagSerializers(many = True)
     class Meta:
         model = Project
         fields = '__all__'
